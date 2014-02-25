edbe6799913ac5779f8eba54ac48a1a36b0e7150
Ed Giardina <ed@litmus.com>
2014-02-25 09:59:50 -0500

    Added helper class to store user's auth data

---
+++ App_Code/AuthHelper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.WebPages;
+using Microsoft.Ajax.Utilities;
+
+/// <summary>
+/// Simple static helper to provide username/password to the example app.
+/// Fill in your own username / password here.
+/// </summary>
+public class AuthHelper
+{
+    /// <summary>
+    /// Edit this value with your Previews API username
+    /// </summary>
+    public static string ApiUsername = "";
+    /// <summary>
+    /// Edit this value with your Previews API password
+    /// </summary>
+    public static string ApiPassword = "";
+
+    static AuthHelper()
+    {
+        if (ApiUsername.IsEmpty() || ApiPassword.IsEmpty())
+            throw new Exception("Please make sure to edit /App_Code/AuthHelper.cs to provide your litmus username and password");
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+This is an example web site demonstrating how to use the Litmus Previews API
+
+Full documentation of the API can be found here: https://previews-api.litmus.com/
+
+For general help, see https://litmus.com/help/
